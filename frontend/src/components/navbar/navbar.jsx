3e96f32cfdc059d46c6a1ad96ab0226874751d39
nobreeu <matheusnobrefe@gmail.com>
2024-12-14 13:28:33 -0300

    Primeira versão navbar

---
+++ frontend/src/components/navbar/navbar.jsx
@@ -0,0 +1,22 @@
+// Shift + Alt + F
+
+import './navbar.css'
+
+function Navbar() {
+    return (
+        <div className="group-14">
+            <div className="rectangle-13"></div>
+            <img className="image-removebg-preview-1" src="image-removebg-preview-10.png" />
+            <div className="cadastrar">Cadastrar</div>
+            <div className="ofertas">Ofertas</div>
+            <div className="trabalhe-conosco">Trabalhe conosco</div>
+            <div className="produtos">Produtos</div>
+            <div className="group-10">
+                <div className="rectangle-1"></div>
+                <div className="buscar-por">Buscar por...</div>
+                <img className="search" src="search0.png" />
+            </div>
+            <div class="line-2"></div>
+        </div>
+    )
+}
