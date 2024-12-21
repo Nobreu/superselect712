0dd032bd8d7fb64b5e6253eac6c49ba4dd12b801
nobreeu <matheusnobrefe@gmail.com>
2024-12-21 16:08:37 -0300

    Cadastro e login backend inicial

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,11 @@
-// config do banco de dados
+const mysql = require('mysql2/promise')
+require('dotenv').config()
 
-// que fase
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+})
+
+module.exports = pool
